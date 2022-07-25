089186f35f291a0ffb4e95d834788510905f1ab9
AlexGruver <alex.gruver@hyperbloom.ai>
2022-07-25 11:33:33 -0400

    contact us page done

--- src/components/ContactUsPage.js
+++ src/components/ContactUsPage.js
@@ -3,8 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 import NavBar from './NavBar'
+import { useState } from 'react'
+
 
 function ContactUs() {
+
+	const [Name, setName] = useState("")
+    const [Email, setEmail] = useState("")
+	const [Subject, setSubject] = useState("")
+    const [Message, setMessage] = useState("")
+
 	return (
 		<>
 			<div>
@@ -16,13 +24,19 @@ function ContactUs() {
 					<body className='contact-us-body'>
 						<ul className='contact-us-ul'>
 							<li className='contact-us-li'>
-								<input className='contact-us-inputs' placeholder='Name'></input>
+								<input className='contact-us-inputs' placeholder='Name' value={Name} onChange={(e) => setName(e.target.value)}></input>
 							</li>
 							<li className='contact-us-li'>
-								<input className='contact-us-inputs' placeholder='username@email.com'></input>
+								<input className='contact-us-inputs' placeholder='yourname@email.com' value={Email} onChange={(e) => setEmail(e.target.value)}></input>
+							</li>
+							<li className='contact-us-li'>
+								<input className='contact-us-inputs' placeholder='Email Subject' value={Subject} onChange={(e) => setSubject(e.target.value)}></input>
+							</li>
+							<li className='contact-us-li'>
+								<textarea className='contact-us-inputs' placeholder='Email Message Here' value={Message} onChange={(e) => setMessage(e.target.value)}></textarea>
 							</li>
 						</ul>
-						<span>Hyperbloom</span>
+						<img className='logo-adjust' src='img/hyperbloom-banner_white_on_black_small.png'></img>
 					</body>
 				</div>
 			</div>
